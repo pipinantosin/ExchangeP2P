8ebed3b904e5c31371e0a62f42ca6d62b7534578
Pipin Antosin <77954264+pipinantosin@users.noreply.github.com>
2026-03-12 22:20:54 +0700

    push zip

---
+++ js/currency-toggle.js
@@ -0,0 +1,26 @@
+const currencySelect = document.getElementById("currencySelect");
+
+currencySelect.addEventListener("change", ()=>{
+
+let value = parseFloat(document.getElementById("sellAmount").value) || 0;
+
+let price = window.PRICE?.SDA || 0; // harga token
+let usdtRate = window.PRICE?.USDT_IDR || 15500;
+
+let rupiah = value * price;
+
+if(currencySelect.value === "idr"){
+
+document.getElementById("rupiahPreview").innerText =
+"Rp " + rupiah.toLocaleString("id-ID");
+
+}else{
+
+let usdt = rupiah / usdtRate;
+
+document.getElementById("rupiahPreview").innerText =
+usdt.toFixed(2);
+
+}
+
+});
