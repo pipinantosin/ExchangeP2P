67b0b1526653bc4860e1b408d9d272e48df44351
Pipin Antosin <77954264+pipinantosin@users.noreply.github.com>
2026-03-17 14:12:41 +0700

    push zip

--- js/app.js
+++ js/app.js
@@ -168,6 +168,17 @@ document.getElementById("clearStorageBtn").onclick = () => {
 
 };
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('sw.js')
+      .then(registration => {
+        console.log("Service Worker registered:", registration.scope);
+      })
+      .catch(err => {
+        console.log("Service Worker registration failed:", err);
+      });
+  });
+}
 
 
 
